c7e2f2c5fcd030599496cf30b1287839d2bb4de5
Nils <nils.programming@gmail.com>
2020-06-25 21:48:00 +0200

    Added animation setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 canvas = document.getElementById("screen")
 screen = canvas.getContext("2d")
 
-screen.fillStyle = "rgb(255,0,0)"
-screen.arc(100, 100, 50, 0, 360)
-screen.fill()
+window.requestAnimationFrame(frame)
+
+function frame() {
+  screen.clearRect(0, 0, canvas.width, canvas.height)
+
+  screen.fillStyle = "rgb(255,0,0)"
+  screen.arc(100, 100, 50, 0, 360)
+  screen.fill()
+
+  window.requestAnimationFrame(frame)
+}
