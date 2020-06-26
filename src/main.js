c8b474d3b1b8bd3c5607d245f791bf68cf76694c
Nils <nils.programming@gmail.com>
2020-06-26 23:32:30 +0200

    Added Flapping by pressing space or touching the screen

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import "/src/bird.js"
 var canvas = document.getElementById("screen")
 var screen = canvas.getContext("2d")
 
+document.addEventListener("keypress", keyPressed)
+canvas.addEventListener("touchstart", touched)
+var flap = false
+
 var fb = new Bird(100, 100)
 
 var t0 = Date.now()
@@ -13,6 +17,11 @@ function frame() {
   let dt = (t1 - t0)/1000
   t0 = t1
 
+  if (flap) {
+    fb.flap(dt)
+    flap = false
+  }
+
   fb.update(dt)
 
   screen.clearRect(0, 0, canvas.width, canvas.height)
@@ -20,4 +29,14 @@ function frame() {
   fb.draw(screen)
 
   window.requestAnimationFrame(frame)
+}
+
+function keyPressed(k) {
+  if (k.code == "Space") {
+    flap = true
+  }
+}
+
+function touched(e) {
+  flap = true
 }
